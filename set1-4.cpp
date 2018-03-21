1c2c803c6fc72e73119d2f80832e5f3d13202738
RhodesRamesh <37578839+RhodesRamesh@users.noreply.github.com>
2018-03-21 12:03:14 +0530

    Create set1-4.cpp

---
+++ set1-4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{char c;
+cout<<"Enter the character:"<<endl;
+cin>>c;
+if((c>='a'&&ch<='z')||(c>='A'&&ch<='Z'))
+{cout<<"Alphabet"<<endl;
+}
+else
+{cout<<" No"<<endl;
+}
+return 0;
+}
